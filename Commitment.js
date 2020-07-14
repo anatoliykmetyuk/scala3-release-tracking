8ee9e80860e9c309fed2faf4757d2e9a7cb09514
Anatolii Kmetiuk <anatoliykmetyuk@gmail.com>
2020-07-14 15:55:58 +0200

    Milestone addition logic: rows

--- Commitment.js
+++ Commitment.js
@@ -1,38 +1,35 @@
+var milestones_sheet = SpreadsheetApp.getActive().getSheetByName("Milestones");
+var commitment_sheet = SpreadsheetApp.getActive().getSheetByName("Commitment");
+
 function commitment_main() {
-  var person = commitment_sheet().getRange("B1")
+  commitment_sheet.getRange("A3:F99").clear();
+
+  var person = commitment_sheet.getRange("B1")
     .getValue();
   console.log("Processing " + person);
 
-  var milestones = milestones_sheet()
+  var milestones = milestones_sheet
     .getRange("A2:A99").getValues();
-  var personCol = milestones_sheet()
+  var personCol = milestones_sheet
     .getRange(1, 5, 1, 99)
     .getValues()[0]
     .findIndex(p => p == person) + 5;
 
   for (r = 0; r < milestones.length; r++) {
     var ms = milestones[r][0];
-    var commitment = milestones_sheet()
+    var commitment = milestones_sheet
       .getRange(r + 2, personCol).getValue();
     if (commitment != "")
       insert_milestone(person, ms, commitment);
   }
 }
 
+var milestones_num = 0;
 function insert_milestone(person, milestone, commitment) {
+  milestones_num += 1;
+  var milestone_row = milestones_num + 2;
+  commitment_sheet.getRange(milestone_row, 1).setValue(milestone);
+
   console.log("Adding " + milestone + " for " +
     person + " with commitment " + commitment);
-}
-
-
-function app() {
-  return SpreadsheetApp.getActive();
-}
-
-function milestones_sheet() {
-  return app().getSheetByName("Milestones");
-}
-
-function commitment_sheet() {
-  return app().getSheetByName("Commitment");
 }
