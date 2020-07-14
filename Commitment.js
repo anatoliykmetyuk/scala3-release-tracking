4127f37c1e3fbfe7ed69f10c80b268504b0223ec
Anatolii Kmetiuk <anatoliykmetyuk@gmail.com>
2020-07-14 15:42:25 +0200

    Milestone addition logic scaffolding

--- Commitment.js
+++ Commitment.js
@@ -1,12 +1,27 @@
 function commitment_main() {
-  var person = commitment_sheet().getRange("B1").getValue();
+  var person = commitment_sheet().getRange("B1")
+    .getValue();
   console.log("Processing " + person);
 
   var milestones = milestones_sheet()
     .getRange("A2:A99").getValues();
-  var personCol = milestones_sheet().getRange(1, 5, 1, 99)
-    .getValues()[0].findIndex(p => p == person) + 5;
-  console.log(person + " has col index " + personCol);
+  var personCol = milestones_sheet()
+    .getRange(1, 5, 1, 99)
+    .getValues()[0]
+    .findIndex(p => p == person) + 5;
+
+  for (r = 0; r < milestones.length; r++) {
+    var ms = milestones[r][0];
+    var commitment = milestones_sheet()
+      .getRange(r + 2, personCol).getValue();
+    if (commitment != "")
+      insert_milestone(person, ms, commitment);
+  }
+}
+
+function insert_milestone(person, milestone, commitment) {
+  console.log("Adding " + milestone + " for " +
+    person + " with commitment " + commitment);
 }
 
 
